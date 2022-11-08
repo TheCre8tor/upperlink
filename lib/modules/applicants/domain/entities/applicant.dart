39f8033a636d48e4f810864ed3c13bd7464ffab3
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-08 14:01:21 +0100

    Created the Applicant domain entity

---
+++ lib/modules/applicants/domain/entities/applicant.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class Applicant extends Equatable {
+  final String firstName;
+  final String surname;
+  final int phoneNumber;
+  final String email;
+  final String coverLetter;
+  final Uint8List passport;
+  final Uint8List resume;
+
+  const Applicant({
+    required this.firstName,
+    required this.surname,
+    required this.phoneNumber,
+    required this.email,
+    required this.coverLetter,
+    required this.passport,
+    required this.resume,
+  });
+
+  @override
+  List<Object?> get props => [
+        firstName,
+        surname,
+        phoneNumber,
+        email,
+        coverLetter,
+        passport,
+        resume,
+      ];
+}
