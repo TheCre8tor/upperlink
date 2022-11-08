f8638081b17dd5bce8793a0c0d2ecc664f25aa2a
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-08 14:40:34 +0100

    Created applicant base repository

---
+++ lib/modules/applicants/repository/applicant_repository.interface.dart
@@ -0,0 +1,8 @@
+import 'package:ruqe/ruqe.dart';
+import 'package:upperlink/modules/applicants/domain/domain.dart';
+
+typedef AppError = String;
+
+abstract class BaseApplicantRepository {
+  Future<Result<void, AppError>> registerApplicant(Applicant applicant);
+}
